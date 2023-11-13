60da755151bfb2f2fd085eca2fc7745c099416bc
BKarayilmaz <berkankarayilmaz@gmail.com>
2023-11-13 22:40:23 +0300

    Add project files.

---
+++ DockerizeNetCoreApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerizeNetCoreApp.Controllers
+{
+    [ApiController]
+    [Route("/test")]
+    public class WeatherForecastController : ControllerBase
+    {
+       
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Dockerize .Net Core API 1.0";
+        }
+    }
+}
